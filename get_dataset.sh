c121902d04fb99a2530a65b01a8085e662c98b5b
cs231n <cstwothirtyonen@gmail.com>
2017-05-13 21:35:48 +0000

    added dataset script and installs

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,5 +1,20 @@
 mkdir ~/final/dataset
-wget http://memorability.csail.mit.edu/lamem.tar.gz
 cd ~/final/dataset
-tar -xzvf lamem.tar.gz
-rm cifar-10-python.tar.gz
+kg download -u cs231n2017 -p cs231n2017 -c invasive-species-monitoring
+
+mkdir ~/final/dataset/train
+cd ~/final/dataset/train
+7za e ../train.7z
+
+mkdir ~/final/dataset/test
+cd ~/final/dataset/test
+7za e ../test.7z
+
+mkdir ~/final/dataset/labels
+cd ~/final/dataset/labels
+unzip ../train_labels.csv.zip
+
+mkdir ~/final/dataset/submission
+cd ~/final/dataset/submission
+unzip ../sample_submission.csv.zip
+
---
+++ installs.sh
@@ -0,0 +1,3 @@
+pip install kaggle-cli
+sudo apt-get install p7zip-full
+sudo apt-get install unzip
