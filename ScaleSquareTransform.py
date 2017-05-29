e08df31690f7d6a983f5c8f6fb5d30e35994e8a5
cs231n <cstwothirtyonen@gmail.com>
2017-05-29 07:33:19 +0000

    experimented with image resizing

---
+++ ScaleSquareTransform.py
@@ -0,0 +1,16 @@
+import torch
+from PIL import Image, ImageOps
+import collections
+
+class ScaleSquare(object):
+    """Rescales the input PIL.Image to the given 'size'.
+    size: size of both edges
+    interpolation: Default: PIL.Image.BILINEAR
+    """
+    def __init__(self, size, interpolation=Image.ANTIALIAS):
+        assert isinstance(size, int)
+        self.size = size
+        self.interpolation = interpolation
+
+    def __call__(self, img):
+        return img.resize((self.size, self.size), self.interpolation)
