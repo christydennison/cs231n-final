00ef1e17d21dcc0f417ad3645b09b3c5bb8d70a5
cs231n <cstwothirtyonen@gmail.com>
2017-06-11 03:07:21 +0000

    changes

--- ImageFolderWithName.py
+++ ImageFolderWithName.py
@@ -46,20 +46,11 @@ def pil_loader(path):
 
 
 def accimage_loader(path):
-    import accimage
-    try:
-        return accimage.Image(path)
-    except IOError:
-        # Potentially a decoding problem, fall back to PIL.Image
-        return pil_loader(path)
+    return pil_loader(path)
 
 
 def default_loader(path):
-    from torchvision import get_image_backend
-    if get_image_backend() == 'accimage':
-        return accimage_loader(path)
-    else:
-        return pil_loader(path)
+    return pil_loader(path)
 
 
 class ImageFolderWithName(data.Dataset):
